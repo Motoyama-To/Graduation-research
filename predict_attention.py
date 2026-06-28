5e234eaac7af2dfad893b3232addddc1916f8c6f
Motoyama-To <3CDA1226@tokai.ac.jp>
2026-06-28 22:10:35 +0900

    Update predict_attention.py

--- predict_attention.py
+++ predict_attention.py
@@ -1,5 +1,15 @@
 # 猫+犬 品種分類・類似画像検索プログラム
 
-# モデル・データ読み込み -- 類似画像検索 -- Wikipedia説明 
+# モデル・データ読み込み -- YOLO切り抜き -- model.eval() -- softmax 
+# -- 類似画像検索 -- Wikipedia説明 
 
 # predict_attention.py
+
+
+model = BreedClassifier(num_classes=10)
+
+model.load_state_dict(
+    torch.load("cat_model.pth", map_location=device)
+)
+
+model.eval()
